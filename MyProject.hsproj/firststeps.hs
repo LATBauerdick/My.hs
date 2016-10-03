a6ba660ed73eb91866f9930b6a7d5defec64c1a0
LATBauerdick <github@latbauerdick.de>
2016-10-03 11:32:55 -0500

    chapter 8

--- MyProject.hsproj/firststeps.hs
+++
@@ -1,8 +0,0 @@
-inc :: Int -> Int     -- type signature
-inc x = x + 1         -- function equation
-
-exclaim :: String -> String
-exclaim sentence = sentence ++ "!"
-
-average :: Float -> Float -> Float
-average a b = (a + b) / 2
